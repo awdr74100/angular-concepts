7780ab88241decf86d88bcbefd6654da87b5f740
Roya <a78945612385238@gmail.com>
2025-07-20 04:54:06 +0800

    feat: inline svg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,9 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAngularSvgIcon } from 'angular-svg-icon';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
+    provideAngularSvgIcon(),
   ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Button } from './shared/components/button/button';
 import { Header } from './shared/components/header/header';
 import { Counter } from './shared/components/counter/counter';
 import { environment } from '../environments/environment';
+// import { InlineSVGComponent } from "ng-inline-svg-2/lib_commonjs/inline-svg.component";
+
+// import { SvgIconComponent } from '@ngneat/svg-icon';
+// import { SvgIconComponent } from "../../node_modules/.pnpm/angular-svg-icon@19.1.1_@an_0e1c5c0e16b69a6a074482848888bcc1/node_modules/angular-svg-icon/lib/svg-icon.component";
+import { SvgIconComponent, SvgIconRegistryService } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button, Header, Counter],
+  imports: [RouterOutlet, Button, Header, Counter, SvgIconComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -15,6 +20,11 @@ export class App implements OnInit {
   protected readonly title = signal('Signal Title');
   message = 'Hello World';
   buttonText = signal('Cancel');
+  iconReg = inject(SvgIconRegistryService);
+
+  constructor() {
+    this.iconReg.loadSvg('/icons/user.svg', 'user')?.subscribe();
+  }
 
   keyUpHandler(event: KeyboardEvent) {
     console.log(event);
