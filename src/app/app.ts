11a06132f92ae8b57b0095f22e41f0da1e3ede8e
Roya <a78945612385238@gmail.com>
2025-07-19 18:03:06 +0800

    chore: 新增 ESLint Prettier 設置

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Button } from './shared/components/button/button';
 import { Header } from './shared/components/header/header';
-import { Counter } from "./shared/components/counter/counter";
+import { Counter } from './shared/components/counter/counter';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,16 @@ import { Counter } from "./shared/components/counter/counter";
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Signal Title');
   message = 'Hello World';
   buttonText = signal('Cancel');
 
   keyUpHandler(event: KeyboardEvent) {
     console.log(event);
+  }
+
+  ngOnInit(): void {
+    console.log(environment.apiUrl);
   }
 }
